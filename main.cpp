a130d35df8ce1e23909d149b21b97758b5d4d291
heydar <heydar.mahmoodi75@gmail.com>
2020-04-16 13:03:19 -0400

    Defualt default constructor in modern C++

--- main.cpp
+++ main.cpp
@@ -7,13 +7,23 @@ private:
     double width{1};
     double height{1};
 public:
+    Box(double l,double w, double h)
+    {
+        length=l;
+        width=w;
+        height=h;
+    }
+    Box()=default; // you can use box(){} too but it's better to use default keyword in modern C++.
     double volume(){return length*height*width;}
 
 };
 
 int main()
 {
-    Box one;
+    Box one(10,2,3);
+    Box two;        // if you didnt define default default constructor , you get error.
+                    // and it's better use default keyword(line 16) , its preferedin modern C++.
     std::cout<< one.volume() <<std::endl;
+    std::cout<<two.volume()<<std::endl;
     return 0;
 }
