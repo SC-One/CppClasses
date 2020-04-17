e0c92c7227132d996abe16d434567c4204ff898c
heydar <heydar.mahmoodi75@gmail.com>
2020-04-16 23:24:23 -0400

    member initializer list in constructors

--- Box.cpp
+++ Box.cpp
@@ -1,11 +1,10 @@
 #include "Box.h"
 
 
-Box::Box(double l, double w, double h)
+Box::Box(double l, double w, double h):length{l},width{w},height{h} // member initializer list.
 {
-    length=l;
-    width=w;
-    height=h;
+    // body code eliminated by member initializer list.
+    // Note: member initializer list is better than assignment , because in assignment the object at first creating and after that assignment carry out.
 }
 
 double Box::volume()
